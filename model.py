102981f3229ba56c397ea9c3a74721439f4e131e
Aridai Bordón <aridai.bordon@ulpgc.es>
2025-03-13 09:24:53 +0000

    update: add get_chi2_scale_factor

--- model.py
+++ model.py
@@ -5,3 +5,8 @@ def get_chi2(x, y, x_sample, y_sample):
     y = np.interp(x_sample, x, y)
     a = np.sum(y * y_sample) / np.sum(y**2)
     return sum((y_sample - a * y) ** 2) / len(y_sample)
+
+
+def get_chi2_scale_factor(x, y, x_sample, y_sample):
+    y = np.interp(x_sample, x, y)
+    return np.sum(y * y_sample) / np.sum(y**2)
